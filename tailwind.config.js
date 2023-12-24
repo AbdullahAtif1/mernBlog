a71fe708eaca67bb47441346529ecd39eb047d47
AbdullahAtif1 <91042316+AbdullahAtif1@users.noreply.github.com>
2023-12-24 19:55:37 +0500

    something

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ejs}"],
+  content: ["./views/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
